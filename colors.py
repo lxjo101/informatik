6111031b3677e24c94efec7cc94e7bf043baedc5
Levi <levischulz06@gmail.com>
2024-07-05 13:50:13 +0200

    Farben

---
+++ colors.py
@@ -0,0 +1,35 @@
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m' # orange on some systems
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+LIGHT_GRAY = '\033[37m'
+DARK_GRAY = '\033[90m'
+BRIGHT_RED = '\033[91m'
+BRIGHT_GREEN = '\033[92m'
+BRIGHT_YELLOW = '\033[93m'
+BRIGHT_BLUE = '\033[94m'
+BRIGHT_MAGENTA = '\033[95m'
+BRIGHT_CYAN = '\033[96m'
+WHITE = '\033[97m'
+
+RESET = '\033[0m' # called to return to standard terminal text color
+
+print(BLACK + "black" + RESET)
+print(RED + "red" + RESET)
+print(GREEN + "green" + RESET)
+print(YELLOW + "yellow" + RESET)
+print(BLUE + "blue" + RESET)
+print(MAGENTA + "magenta" + RESET)
+print(CYAN + "cyan" + RESET)
+print(LIGHT_GRAY + "light gray" + RESET)
+print(DARK_GRAY + "dark gray" + RESET)
+print(BRIGHT_RED + "bright red" + RESET)
+print(BRIGHT_GREEN + "bright green" + RESET)
+print(BRIGHT_YELLOW + "bright yellow" + RESET)
+print(BRIGHT_BLUE + "bright blue" + RESET)
+print(BRIGHT_MAGENTA + "bright magenta" + RESET)
+print(BRIGHT_CYAN + "bright cyan" + RESET)
+print(WHITE + "white" + RESET)
